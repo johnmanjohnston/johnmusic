ff8c6afec3823f3e071d0113d93dc611ac117b90
johnmanjohnston <c.ethanjohn@gmail.com>
2023-09-24 17:42:23 +0400

    Optimizations, changing songs dir functionality

--- johnmusic/johnmusic.cpp
+++ johnmusic/johnmusic.cpp
@@ -5,10 +5,12 @@
 #include <conio.h>
 #include <vector>
 #include <filesystem>
+#include <fstream>
 
 #pragma comment (lib, "winmm.lib")
 
-std::string SONGS_DIR = "C:/Users/USER/OneDrive/Desktop/new-world/";
+std::string CURRENT_SONGS_DIR;
+std::vector<std::string> SONGS_DIRS_CHOICES;
 std::vector<std::string> SONGS_LIST;
 bool IS_PLAYING = false;
 LPCWSTR CURRENT_SONG_PATH;
@@ -16,32 +18,62 @@ int curentSongIndex = 0;
 
 void Set_SONGS_LIST() 
 {
-    std::filesystem::directory_iterator directoryScanner(SONGS_DIR);
+    SONGS_LIST.clear();
+    std::cout << "Loading songs from " << CURRENT_SONGS_DIR << std::endl;
+    std::filesystem::directory_iterator directoryScanner(CURRENT_SONGS_DIR);
     for (auto& entry : directoryScanner) {
         if (entry.is_regular_file()) {
             std::string fname = entry.path().filename().string();
             SONGS_LIST.push_back(fname);
         }   
     }
+
+    std::cout << "Loaded " << SONGS_LIST.size() << " songs\n";
+}
+
+void Set_SONGS_DIRS_CHOICES() 
+{
+    std::ifstream songsDirPathFile("songs_dir.txt");
+    if (songsDirPathFile.fail()) {
+        std::cout << "Couldn't locate songs_dir.txt\n";
+        exit(1);
+    }
+
+    std::string currentLine;
+
+    while (std::getline(songsDirPathFile, currentLine)) {
+        SONGS_DIRS_CHOICES.push_back(currentLine);
+        if (CURRENT_SONGS_DIR == "") {
+            CURRENT_SONGS_DIR = currentLine;
+        }
+    }
+
+    songsDirPathFile.close();
 }
 
 void LogTutorial() 
 {
-    std::cout << "\nUsage:\np - toggle play/pause\nlist - list songs\ns<index: int> - plays song from index\nkill/abort/exit - terminate\nhelp - launches an atomic bomb at your house\n\n";
+    std::cout << "\nUsage:\np - toggle play/pause\nlist - list songs\ns<index: int> - plays song from index\ncd<index: int> - loads songs from different directory specified by index\nkill/abort/exit - terminate\nhelp - launches an atomic bomb at your house\n\n";
 }
 
-void ListSongs() {
+void ListSongsAndSongDirectories() {
+    std::cout << "Songs: \n";
     for (int i = 0; i < SONGS_LIST.size(); ++i) {
         std::cout << i << " " << SONGS_LIST[i] << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Directories: \n";
+    for (int i = 0; i < SONGS_DIRS_CHOICES.size(); ++i) {
+        std::cout << i << " " << SONGS_DIRS_CHOICES[i] << std::endl;
     }
 }
 
 int main()
 {
+    Set_SONGS_DIRS_CHOICES();
     Set_SONGS_LIST();
-
-    std::cout << "Loading songs from " << SONGS_DIR << std::endl;
-    std::cout << "Loaded " << SONGS_LIST.size() << " songs" << std::endl;
 
     LogTutorial();
 
@@ -55,18 +87,18 @@ int main()
         }
 
         if (cmd == "list") {
-            ListSongs();
+            ListSongsAndSongDirectories();
         }
 
         if (cmd == "p") {
             IS_PLAYING = !IS_PLAYING;
 
             if (!IS_PLAYING) {
-                PlaySound(NULL, NULL, SND_ASYNC);
+                PlaySound(NULL, NULL, 0);
             }
             else {
                 std::string songName = SONGS_LIST[curentSongIndex];
-                std::string songPath = SONGS_DIR + songName;
+                std::string songPath = CURRENT_SONGS_DIR + songName;
                 std::wstring temp = std::wstring(songPath.begin(), songPath.end());
                 CURRENT_SONG_PATH = temp.c_str();
                 PlaySound(CURRENT_SONG_PATH, NULL, SND_FILENAME | SND_ASYNC);
@@ -75,17 +107,23 @@ int main()
         }
 
         if (cmd.rfind("s", 0) == 0) {
-            int songIndex = std::stoi(cmd.substr(1));
-            curentSongIndex = songIndex;
-            std::string songName = SONGS_LIST[songIndex];
-            std::string songPath = SONGS_DIR + songName;
+            curentSongIndex = std::stoi(cmd.substr(1));
+            std::string songName = SONGS_LIST[curentSongIndex];
+            std::string songPath = CURRENT_SONGS_DIR + songName;
             std::wstring temp = std::wstring(songPath.begin(), songPath.end());
             CURRENT_SONG_PATH = temp.c_str();
 
             std::cout << songPath;
 
-            PlaySound(CURRENT_SONG_PATH, NULL, SND_FILENAME | SND_ASYNC);
+            auto b = PlaySound(CURRENT_SONG_PATH, NULL, SND_FILENAME | SND_ASYNC);
+            std::cout << b;
             IS_PLAYING = true;
+        }
+
+        if (cmd.rfind("cd", 0) == 0) {
+            int dirIndex = std::stoi(cmd.substr(2));
+            CURRENT_SONGS_DIR = SONGS_DIRS_CHOICES[dirIndex];
+            Set_SONGS_LIST();
         }
 
         if (cmd == "help") {
